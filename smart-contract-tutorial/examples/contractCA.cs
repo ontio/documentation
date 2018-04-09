70c8df84c140ec4e889bdaaeb1eab1cdda6f3a9f
chris <tcpdumpp@163.com>
2018-04-09 18:48:26 +0800

    Create contractCA.cs
    add smart contract CA template, this template provide two fuction: 
    Put(k, v) - set key k with value v
    Get(k) - Get key k value

---
+++ smart-contract-tutorial/examples/contractCA.cs
@@ -0,0 +1,55 @@
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
+using System;
+using System.ComponentModel;
+using System.Numerics;
+
+namespace Ontology
+{
+    public class Ontology : SmartContract
+    {
+        [DisplayName("putRecord")]
+        public static event Action<string, byte[], byte[]> PutEvent;
+
+        [DisplayName("getRecord")]
+        public static event Action<string, byte[]> GetEvent;
+
+        public static Object Main(string operation, params object[] args)
+        {
+            if (Runtime.Trigger == TriggerType.Application)
+            {
+                if (operation == "Put")
+                {
+                    if (args.Length != 2) return false;
+                    byte[] key = (byte[])args[0];
+                    byte[] value = (byte[])args[1];
+                    return Put(key, value);
+                }
+
+                if (operation == "Get")
+                {
+                    if (args.Length != 1) return false;
+                    byte[] key = (byte[])args[0];
+                    return Get(key);
+                }
+            }
+            return false;
+        }
+
+        public static bool Put(byte[] key, byte[] value)
+        {
+            Storage.Put(Storage.CurrentContext, key, value);
+            PutEvent("Put", key, value);
+            Runtime.Log("Put");
+            return true;
+        }
+
+        public static byte[] Get(byte[] key)
+        {
+            GetEvent("Get", key);
+            Runtime.Log("Get");
+            return Storage.Get(Storage.CurrentContext, key);
+        }
+    }
+}
