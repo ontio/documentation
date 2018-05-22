05ee7fe0750f9add544fe9e935727d947ba05b14
chris <tcpdumpp@163.com>
2018-05-22 18:03:21 +0800

    Update domain.py
    Neo-boa system api changes, the smartcontct imported package need to change too

--- smart-contract-tutorial/examples/domain.py
+++ smart-contract-tutorial/examples/domain.py
@@ -1,13 +1,12 @@
-from boa.blockchain.vm.Neo.Runtime import Log, Notify
-from boa.blockchain.vm.System.ExecutionEngine import GetScriptContainer, GetExecutingScriptHash
-from boa.blockchain.vm.Neo.Transaction import *
-from boa.blockchain.vm.Neo.Blockchain import GetHeight, GetHeader
-from boa.blockchain.vm.Neo.Action import RegisterAction
-from boa.blockchain.vm.Neo.Runtime import GetTrigger, CheckWitness
-from boa.blockchain.vm.Neo.TriggerType import Application, Verification
-from boa.blockchain.vm.Neo.Output import GetScriptHash, GetValue, GetAssetId
-from boa.blockchain.vm.Neo.Storage import GetContext, Get, Put, Delete
-from boa.blockchain.vm.Neo.Header import GetTimestamp, GetNextConsensus
+from boa.interop.Neo.Runtime import Log, GetTrigger, CheckWitness
+from boa.interop.System.ExecutionEngine import GetScriptContainer, GetExecutingScriptHash
+from boa.interop.Neo.Transaction import *
+from boa.interop.Neo.Blockchain import GetHeight, GetHeader
+from boa.interop.Neo.Action import *
+from boa.interop.Neo.Output import GetScriptHash, GetValue, GetAssetId
+from boa.interop.Neo.Storage import GetContext, Get, Put, Delete
+from boa.interop.Neo.Header import GetTimestamp, GetNextConsensus
+
 
 Push = RegisterAction('event', 'operation', 'msg')
 
